3e37c2865b69089d51ff217a5e145127e4667f8b
jfbj54-arch <SEU_EMAIL_DO_GITHUB>
2026-07-14 11:27:56 -0300

    Migration Initial limpa

---
+++ Migrations/20260714142749_Initial.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RSConnect.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "prestadores",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    senha = table.Column<string>(type: "text", nullable: false),
+                    telefone = table.Column<string>(type: "text", nullable: false),
+                    categoria = table.Column<string>(type: "text", nullable: false),
+                    endereco = table.Column<string>(type: "text", nullable: false),
+                    nota = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_prestadores", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "servicos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    cliente_id = table.Column<int>(type: "integer", nullable: false),
+                    prestador_id = table.Column<int>(type: "integer", nullable: true),
+                    categoria = table.Column<string>(type: "text", nullable: false),
+                    descricao = table.Column<string>(type: "text", nullable: false),
+                    endereco = table.Column<string>(type: "text", nullable: false),
+                    data_hora = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_servicos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    senha = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "prestadores");
+
+            migrationBuilder.DropTable(
+                name: "servicos");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
