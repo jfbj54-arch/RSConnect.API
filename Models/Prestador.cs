242ab06b157533b3231bbf8888be2328c9139bef
jfbj54-arch <SEU_EMAIL_DO_GITHUB>
2026-07-14 17:40:43 -0300

    Fix: adicionando email e senha ao modelo Prestador

--- Models/Prestador.cs
+++ Models/Prestador.cs
@@ -6,5 +6,9 @@ namespace RSConnect.API.Models
         public string nome { get; set; } = string.Empty;
         public string telefone { get; set; } = string.Empty;
         public string categoria { get; set; } = string.Empty;
+
+        // CAMPOS NECESSÁRIOS PARA LOGIN
+        public string email { get; set; } = string.Empty;
+        public string senha { get; set; } = string.Empty;
     }
 }
